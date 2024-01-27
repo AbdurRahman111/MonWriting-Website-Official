0e2f53f2396c67c05c1755b2f158fbaa6f436e5b
AbdurRahman111 <mdabdurrahmanchowdhury1122@gmail.com>
2024-01-28 03:05:15 +0600

    add 12 tools and updated the tools page and sitemap

--- tools/urls.py
+++ tools/urls.py
@@ -11,4 +11,18 @@ urlpatterns = [
     path('Distance-Converter/', views.Distance_Converter, name="Distance_Converter"),
     path('Time-Coding/', views.Time_Coding, name="Time_Coding"),
     path('Create-your-Own-Name-QR-code-for-Free/', views.Create_your_Own_Name_QR_code_for_Free, name="Create_your_Own_Name_QR_code_for_Free"),
+
+    path('YouTube-Thumbnail-Downloader/', views.YouTube_Thumbnail_Downloader, name="YouTube_Thumbnail_Downloader"),
+    path('Word-and-Character-Counter-Tool/', views.Word_and_Character_Counter_Tool, name="Word_and_Character_Counter_Tool"),
+    path('About-Us-Page-Generator-for-Website/', views.About_Us_Page_Generator_for_Website, name="About_Us_Page_Generator_for_Website"),
+    path('Password-generator-zone/', views.Password_generator_zone, name="Password_generator_zone"),
+    path('Active-Voice-to-Passive-Voice-Converter/', views.Active_Voice_to_Passive_Voice_Converter, name="Active_Voice_to_Passive_Voice_Converter"),
+    path('BMI-Calculator/', views.BMI_Calculator, name="BMI_Calculator"),
+    path('Flower-Coding/', views.Flower_Coding, name="Flower_Coding"),
+    path('Pregnancy-Calculator/', views.Pregnancy_Calculator, name="Pregnancy_Calculator"),
+    path('Currency-Converter-Tool/', views.Currency_Converter_Tool, name="Currency_Converter_Tool"),
+    path('PNG-to-JPG-Image/', views.PNG_to_JPG_Image, name="PNG_to_JPG_Image"),
+    path('Age-Calculate-Calculator/', views.Age_Calculate_Calculator, name="Age_Calculate_Calculator"),
+    path('Area-Calculate-Calculator/', views.Area_Calculate_Calculator, name="Area_Calculate_Calculator"),
+    path('Percentage-Calculate-Calculator/', views.Percentage_Calculate_Calculator, name="Percentage_Calculate_Calculator"),
 ]
--- tools/views.py
+++ tools/views.py
@@ -23,3 +23,42 @@ def Time_Coding(request):
 
 def Create_your_Own_Name_QR_code_for_Free(request):
     return render(request, 'tools/Create_your_Own_Name_QR_code_for_Free.html')
+
+def YouTube_Thumbnail_Downloader(request):
+    return render(request, 'tools/YouTube_Thumbnail_Viewer.html')
+
+def Word_and_Character_Counter_Tool(request):
+    return render(request, 'tools/Word_and_Character_Counter_Tool.html')
+
+def About_Us_Page_Generator_for_Website(request):
+    return render(request, 'tools/About_Us_Page_Generator_for_Website.html')
+
+def Password_generator_zone(request):
+    return render(request, 'tools/Password_generator_zone.html')
+
+def Active_Voice_to_Passive_Voice_Converter(request):
+    return render(request, 'tools/Active_Voice_to_Passive_Voice_Converter.html')
+
+def BMI_Calculator(request):
+    return render(request, 'tools/BMI_Calculator.html')
+
+def Flower_Coding(request):
+    return render(request, 'tools/Flower_Coding.html')
+
+def Pregnancy_Calculator(request):
+    return render(request, 'tools/Pregnancy_Calculator.html')
+
+def Currency_Converter_Tool(request):
+    return render(request, 'tools/Currency_Converter_Tool.html')
+
+def PNG_to_JPG_Image(request):
+    return render(request, 'tools/PNG_to_JPG_Image.html')
+
+def Age_Calculate_Calculator(request):
+    return render(request, 'tools/Age_Calculate_Calculator.html')
+
+def Area_Calculate_Calculator(request):
+    return render(request, 'tools/Area_Calculate_Calculator.html')
+
+def Percentage_Calculate_Calculator(request):
+    return render(request, 'tools/Percentage_Calculate_Calculator.html')
