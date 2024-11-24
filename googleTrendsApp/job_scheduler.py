e8f09d6c1a0979d269e6be48252f5dba9b66fa15
abdurrahmanc-dev <abdurrahmanc@lambdacrunch.com>
2024-11-24 13:29:58 +0600

    keyword tags added2

--- googleTrendsApp/job_scheduler.py
+++ googleTrendsApp/job_scheduler.py
@@ -21,7 +21,7 @@ def start():
     # scheduler.add_job(execute_update_status_change_every_hour, 'interval', seconds=100)
     # scheduler.add_job(execute_update_status_change_every_hour, 'interval', minutes=5, id=f'{datetime.now()}', max_instances=1)
     # scheduler.add_job(execute_update_status_change_every_hour, 'interval', hours=1)
-    scheduler.add_job(execute_save_trends_news_everyday, 'cron', day_of_week='mon-sun', hour=13, minute=28)
+    scheduler.add_job(execute_save_trends_news_everyday, 'cron', day_of_week='mon-sun', hour=16, minute=30)
     scheduler.start()
 
 # Function to check if there are any cheques in queue approaching clearance date in the current week
