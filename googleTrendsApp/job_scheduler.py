47071e6f03e3a01a57219cacef43c2173f0f7729
abdurrahmanc-dev <abdurrahmanc@lambdacrunch.com>
2024-12-07 02:18:02 +0600

    set new time

--- googleTrendsApp/job_scheduler.py
+++ googleTrendsApp/job_scheduler.py
@@ -21,8 +21,10 @@ def start():
     # scheduler.add_job(execute_update_status_change_every_hour, 'interval', seconds=100)
     # scheduler.add_job(execute_update_status_change_every_hour, 'interval', minutes=5, id=f'{datetime.now()}', max_instances=1)
     # scheduler.add_job(execute_update_status_change_every_hour, 'interval', hours=1)
-    scheduler.add_job(execute_save_trends_news_everyday, 'cron', day_of_week='mon-sun', hour=22, minute=0)
-    scheduler.add_job(execute_save_trends_news_everyday, 'cron', day_of_week='mon-sun', hour=2, minute=0)
+    scheduler.add_job(execute_save_trends_news_everyday, 'cron', day_of_week='mon-sun', hour=6, minute=0)
+    scheduler.add_job(execute_save_trends_news_everyday, 'cron', day_of_week='mon-sun', hour=12, minute=0)
+    scheduler.add_job(execute_save_trends_news_everyday, 'cron', day_of_week='mon-sun', hour=18, minute=0)
+    scheduler.add_job(execute_save_trends_news_everyday, 'cron', day_of_week='mon-sun', hour=0, minute=0)
     scheduler.start()
 
 # Function to check if there are any cheques in queue approaching clearance date in the current week
